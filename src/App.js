7fb283d23bbb2214d68f04592ced11876eb848f8
mikolajmialkowski <mikolaj.mialkowski@outlook.com>
2022-06-06 11:30:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseIteam from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 69,
+      date: new Date(),
+    },
+    {
+      title: "Boat Insurance",
+      amount: 420,
+      date: new Date(),
+    },
+    {
+      title: "Plane Insurance",
+      amount: 2137,
+      date: new Date(),
+    },
+    {
+      title: "Rocket Insurance",
+      amount: 6969,
+      date: new Date(),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseIteam
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseIteam>
+      <ExpenseIteam
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseIteam>
+      <ExpenseIteam
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseIteam>
+      <ExpenseIteam
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseIteam>
     </div>
   );
 }
