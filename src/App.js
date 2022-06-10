1549a71a2ec958a6d402fcac13e9fdb8f0db49ee
mikolajmialkowski <mikolaj.mialkowski@outlook.com>
2022-06-10 11:33:20 +0200

    added dynamic list

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    title: "Car Insurance",
+    amount: 69,
+    date: new Date(),
+  },
+  {
+    title: "Boat Insurance",
+    amount: 420,
+    date: new Date(),
+  },
+  {
+    title: "Plane Insurance",
+    amount: 2137,
+    date: new Date(),
+  },
+  {
+    title: "Rocket Insurance",
+    amount: 6969,
+    date: new Date(),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      title: "Car Insurance",
-      amount: 69,
-      date: new Date(),
-    },
-    {
-      title: "Boat Insurance",
-      amount: 420,
-      date: new Date(),
-    },
-    {
-      title: "Plane Insurance",
-      amount: 2137,
-      date: new Date(),
-    },
-    {
-      title: "Rocket Insurance",
-      amount: 6969,
-      date: new Date(),
-    },
-  ];
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES) 
 
   const addExpenseHandler = (expenseData) => {
-      console.log("In App.js");
-      console.log(expenseData);
+    setExpenses((previousState) => {return [expenseData,...previousState]} )
   };
 
   return (
@@ -37,6 +40,6 @@ const App = () => {
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
