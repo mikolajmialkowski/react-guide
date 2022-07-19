81d5a8ab027850eaf47c0fc95ff483168c1f9dcf
mikolajmialkowski <mikolaj.mialkowski@outlook.com>
2022-07-19 13:19:10 +0200

    I am smart or something

--- src/components/Expenses/ExpenseChart.js
+++ src/components/Expenses/ExpenseChart.js
@@ -5,60 +5,74 @@ const ExpenseChart = (props) => {
     {
       label: "Jan",
       value: 0,
+      number: 0,
     },
     {
       label: "Feb",
       value: 0,
+      number: 1,
     },
     {
       label: "Mar",
       value: 0,
+      number: 3,
     },
     {
       label: "Apr",
       value: 0,
+      number: 3,
+    },
+    {
+      label: "May",
+      value: 0,
+      number: 4,
     },
     {
       label: "Jun",
       value: 0,
+      number: 5,
     },
     {
       label: "Jul",
       value: 0,
+      number: 6,
     },
     {
       label: "Aug",
       value: 0,
+      number: 7,
     },
     {
       label: "Sep",
       value: 0,
+      number: 8,
     },
     {
       label: "Oct",
       value: 0,
+      number: 9,
     },
     {
       label: "Nov",
       value: 0,
+      number: 10,
     },
     {
       label: "Dec",
       value: 0,
+      number: 11,
     },
   ];
 
-  for (const expense of props.expenses) {
-    const expenseMonth = expense.date.getMonth();
-    chartDataPoints[expenseMonth].value = chartDataPoints[expenseMonth].value + expense.amount;
-  }
+  chartDataPoints.forEach(
+    (month) =>
+      (month.value = props.expenses
+        .filter((expense) => expense.date.getMonth() === month.number)
+        .map((e) => Number(e.amount))
+        .reduce((a, b) => a + b, 0))
+  );
 
-  
-
-
-
-
-  return <Chart dataPoints={chartDataPoints}></Chart>;
+  return <Chart dataPoints={chartDataPoints} />;
 };
 
 export default ExpenseChart;
